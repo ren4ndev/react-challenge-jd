67af8bbc96e2234d1db1f1ada1bc3acb906257b0
Renan Oliveira <oliveira.renan.rg@gmail.com>
2021-08-26 20:21:46 -0300

    feat: initial structure of the project

---
+++ src/components/Segments/Header/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {
+  HeaderContainer,
+} from './styles';
+
+export default function Header() {
+  return (
+    <HeaderContainer>HeaderContainer</HeaderContainer>
+  );
+}
